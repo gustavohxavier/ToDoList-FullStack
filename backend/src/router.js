4da10c4125f9e19b9d76eb9d3fbcf8565f76e2d2
Gustavo Hidalgo Xavier <guhidalgox@gmail.com>
2022-11-26 09:05:35 -0300

    Config Database connection

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => res.status(200).send('o router tá funcionando!'));
+//router.get('/tasks', (req, res) => res.status(200).send('o router tá funcionando!'));
 
 module.exports = router;
