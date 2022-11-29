94948edb1d212dba5c9ced20a8d2f00113536aab
Gustavo Hidalgo Xavier <guhidalgox@gmail.com>
2022-11-29 09:13:34 -0300

    edit: "MYSLQ" arq names.

--- backend/src/models/connection.js
+++ backend/src/models/connection.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const connection = mysql.createPool({
-    host: process.env.MYSLQ_HOST,
-    user: process.env.MYSLQ_USER,
-    password: process.env.MYSLQ_PASSWORD,
-    database: process.env.MYSLQ_DB
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB
 });
 
 module.exports = connection;
